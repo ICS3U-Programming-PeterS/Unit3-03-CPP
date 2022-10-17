8c3832c28cea8eb0496e3e9662452cf1d5edbcdf
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-10-17 04:33:50 +0000

    Added randNumGuess.cpp

---
+++ randNumGuess.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2022 Peter Sobowale All rights reserved.
+//
+//  This program asks the user for a random number from
+// 0 to 9 and checks if it is the same as the correct
+// number that is generated
+#include <iostream>
+
+#include <random>
+
+int main() {
+    // declare variables
+    int userNumber, randomNumber;
+
+    // get user input for a number form 0 to 9
+    std::cout << "Enter a number from 0 to 9: ";
+    std::cin >> userNumber;
+
+    // initialize random seed
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());
+
+    // generate random number
+    std::uniform_int_distribution<int> idist(0, 9);
+    randomNumber = idist(rgen);
+
+    // checks if user input is equal to random number
+    if (userNumber == randomNumber) {
+        std::cout << "You guessed correctly!";
+    } else {
+        std::cout << "The correct answer was " << (randomNumber);
+    }
+}
